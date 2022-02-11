80d1883abbd75f48cefc79fa458af71cb7358f47
vext <git@vext.co.uk>
2022-02-11 13:18:43 +0000

    update About

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -7,15 +7,17 @@ type Props = {
 
 export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="A-dro" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is an open source word guessing game -{' '}
+        This is an open source version of a word guessing game using Cornish Standard Written Form. 
+<br /><a href="https://github.com/gcrichton/gerdle"  className="underline font-bold"
+>Gerdle</a>
+<br />Based on {' '}
         <a
-          href="https://github.com/hannahcode/GAME"
+          href="https://github.com/cwackerfuss/react-wordle"
           className="underline font-bold"
         >
-          check out the code here
-        </a>{' '}
+          Reactle        </a>{' '}
       </p>
     </BaseModal>
   )
